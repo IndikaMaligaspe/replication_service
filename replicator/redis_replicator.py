9260233f2c411efa87d47f32ea4ba92fa868fa79
IndikaM <indikamalirezg@gmail.com>
2017-05-24 20:37:39 +0530

    added methods for update and delete

--- replicator/redis_replicator.py
+++ replicator/redis_replicator.py
@@ -37,27 +37,69 @@ def main():
         only_events=[DeleteRowsEvent, WriteRowsEvent, UpdateRowsEvent])
     # stream.starting_binlog_pos = 1263
     for binlogevent in stream:
+        schema = binlogevent.schema
+        table = binlogevent.table
+        columns = binlogevent.columns
         prefix = "%s:%s" %(binlogevent.schema,binlogevent.table)
 
         for row in binlogevent.rows:
-            if stream.log_pos >= 1263:
+            # if stream.log_pos >= 1263:
                 if isinstance(binlogevent,DeleteRowsEvent):
+                    where = []
                     vals = row["values"]
+                    where = prepare_where_clause(vals)
                     # query = row["Query"]
-                    print "delete values = %s , prefix = %s , query = (%s)" %(vals , prefix,row)
+                    SQL = 'delete from `'+schema+'`.`'+table+'` where ' + "and".join(where)
+                    print 'Delete SQL : '+SQL
+                    print "delete values = %s , prefix = %s , query = (%s), columns  = (%s)" %(vals , prefix,row,columns)
                     r.delete(prefix+str(vals))
                 elif isinstance(binlogevent,UpdateRowsEvent):
-                    vals = row["after_values"]
-                    # query = row["Query"]
-                    print "update values = %s , prefix = %s , query = (%s)" %(vals , prefix,row)
+                    where = []
+                    update = []
+                    set_vals = row["before_values"]
+                    where_vals = row["after_values"]
+
+                    update = pepare_update_clause(set_vals)
+                    where = prepare_where_clause(where_vals)
+
+                    SQL = 'update  `'+schema+'`.`'+table+'` set '+" , ".join(update)+' where ' + "and".join(where)
+                    print 'Update SQL : '+SQL
+                    print "update values = %s , prefix = %s , query = (%s) , columns  = (%s)" %(vals , prefix,row ,columns)
                     r.hmset(prefix+str(vals),vals)
                 elif isinstance(binlogevent,WriteRowsEvent):
                     vals = row["values"]
                     # query = row["Query"]
-                    print "insert values = %s , prefix = %s , query = (%s)" %(vals , prefix,row)
+                    print "insert values = %s , prefix = %s , query = (%s) , columns  = (%s)" %(vals , prefix,row ,columns)
                     r.hmset(prefix+str(vals),vals)
     print "log position = %s" %(stream.log_pos)
     stream.close()
+
+def prepare_where_clause(vals):
+    where = []
+    for keys in vals:
+        column = vals[keys];
+        if type(column) == int:
+            where.append(' '+keys+'='+str(column)+' ')
+        elif type(column) == bool:
+            where.append(' '+keys+'='+str(column)+' ')
+        else:
+            where.append(' '+keys+'=\''+str(column)+'\''+' ')
+
+    return where
+
+def pepare_update_clause(set_vals):
+    update = []
+    for keys in set_vals:
+        column = set_vals[keys]
+        if type(column) == int:
+            update.append(' '+keys+'='+str(column)+' ')
+        elif type(column) == bool:
+            update.append(' '+keys+'='+str(column)+' ')
+        else:
+            update.append(' '+keys+'=\''+str(column)+'\''+' ')
+
+
+    return update
 
 if __name__ == "__main__":
     main()
