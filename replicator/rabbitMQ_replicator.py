a30f25bfc2649a35fa493a0795ecf79a35e13ae4
IndikaM <indikamalirezg@gmail.com>
2017-05-28 21:41:19 +0530

    added log positioning and master file reading from DB

--- replicator/rabbitMQ_replicator.py
+++ replicator/rabbitMQ_replicator.py
@@ -7,7 +7,10 @@
 
 import pika
 import time
+import logging
+
 from time import sleep
+import mysql.connector
 
 from pymysqlreplication import BinLogStreamReader
 from pymysqlreplication.row_event import(
@@ -20,6 +23,9 @@ from pymysqlreplication.event import (
     RotateEvent
 )
 
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+logger = logging.getLogger()
+
 MYSQL_SETTING = {
     "host": "192.168.50.4",
     "port": 3306,
@@ -28,10 +34,11 @@ MYSQL_SETTING = {
 }
 
 def main():
-    log_position = 0
+    log_position = 4
     start_binlograder(log_position)
 
 def start_binlograder(log_position):
+    log_file = get_latest_master_log_file()
     credentials = pika.PlainCredentials('Indika','changeme')
     connection  = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.50.5',credentials=credentials))
     channel = connection.channel()
@@ -40,73 +47,79 @@ def start_binlograder(log_position):
 
 
     while True:
+        logger.debug('Log position - %s' %(log_position))
         stream = BinLogStreamReader(
             connection_settings=MYSQL_SETTING,
-            # resume_stream=True,
-            server_id=3,
+            resume_stream=True,
+            log_file=log_file,
+            server_id=1,
+            blocking = True,
             log_pos=log_position,
             only_events=[DeleteRowsEvent, WriteRowsEvent, UpdateRowsEvent,QueryEvent,RotateEvent])
-            
+        logger.debug('stream.log_pos -> %s' %(stream.log_pos))
         for binlogevent in stream:
-            if isinstance(binlogevent,RotateEvent):
-                print 'Log Rotated'
-                log_position = 0
-            elif isinstance(binlogevent,QueryEvent):
-                schema = binlogevent.schema
-                prefix = "%s: " %(schema)
-                query = str(binlogevent.query)
-                print ('Query Event - %s' %(query))
-                if "create table".upper() in query.upper():
-                    print ('Create SQL - %s' %(query))
-                    push_mesage(channel,query,queue)
-                    log_position = stream.log_pos
-                elif "drop table".upper() in query.upper():
-                    print ('Drop SQL - %s' %(query))
-                    push_mesage(channel,query,queue)
-                    log_position = stream.log_pos
+            logger.debug('stream.log_pos -> %s' %(stream.log_pos))
+            if stream.log_pos >= log_position:
+                logger.debug('Inside Stream position - %s and log position %s' %(stream.log_pos,log_position))
+                if isinstance(binlogevent,QueryEvent):
+                    schema = binlogevent.schema
+                    prefix = "%s: " %(schema)
+                    query = str(binlogevent.query)
+                    logger.debug ('Query Event - %s' %(query))
+                    if "create table".upper() in query.upper():
+                        logger.debug ('Create SQL - %s' %(query))
+                        push_mesage(channel,query,queue)
+                        log_position = stream.log_pos
+                    elif "drop table".upper() in query.upper():
+                        logger.debug ('Drop SQL - %s' %(query))
+                        push_mesage(channel,query,queue)
+                        log_position = stream.log_pos
 
-            else:
-                schema = binlogevent.schema
-                table = binlogevent.table
-                columns = binlogevent.columns
-                for row in binlogevent.rows:
-                    # if stream.log_pos >= 1263:
-                        if isinstance(binlogevent,DeleteRowsEvent):
-                            prefix = "%s:%s" %(schema,table)
-                            where = []
-                            vals = row["values"]
-                            where = prepare_where_clause(vals)
-                            SQL = 'delete from `'+schema+'`.`'+table+'` where ' + " and ".join(where)
-                            print 'Delete SQL : '+SQL
-                            # r.set(name = prefix+"delete - ", value = SQL)
-                            push_mesage(channel,SQL,queue)
-                            log_position = stream.log_pos
-                        elif isinstance(binlogevent,UpdateRowsEvent):
-                            prefix = "%s:%s" %(schema,table)
-                            where = []
-                            update = []
-                            set_vals = row["after_values"]
-                            where_vals = row["before_values"]
-                            update = prepare_update_clause(set_vals)
-                            where = prepare_where_clause(where_vals)
-                            SQL = 'update  `'+schema+'`.`'+table+'` set '+" , ".join(update)+' where ' + " and ".join(where)
-                            print 'Update SQL : '+SQL
-                            # r.set(name = prefix+"update - ", value = SQL)
-                            push_mesage(channel,SQL,queue)
-                            log_position = stream.log_pos
-                        elif isinstance(binlogevent,WriteRowsEvent):
-                            prefix = "%s:%s" %(schema,table)
-                            vals = row["values"]
-                            insert = prepare_insert_values(vals)
-                            SQL = 'insert into  `'+schema+'`.`'+table+'` ('+" , ".join(insert['cols'])+') values ('+" , ".join(insert['vals'])+')'
-                            print 'Insert SQL : '+SQL
-                            # r.set(name = prefix+"insert - ", value = SQL)
-                            push_mesage(channel,SQL,queue)
-                            log_position = stream.log_pos
+                elif isinstance(binlogevent,RotateEvent):
+                    log_file = get_latest_master_log_file()
+                    log_position = 4
+                else:
+                    schema = binlogevent.schema
+
+                    for row in binlogevent.rows:
+                            table = binlogevent.table
+                            columns = binlogevent.columns
+                            if isinstance(binlogevent,DeleteRowsEvent):
+                                prefix = "%s:%s" %(schema,table)
+                                where = []
+                                vals = row["values"]
+                                where = prepare_where_clause(vals)
+                                SQL = 'delete from `'+schema+'`.`'+table+'` where ' + " and ".join(where)
+                                logger.debug('Delete SQL : '+SQL)
+                                # r.set(name = prefix+"delete - ", value = SQL)
+                                push_mesage(channel,SQL,queue)
+                                log_position = stream.log_pos
+                            elif isinstance(binlogevent,UpdateRowsEvent):
+                                prefix = "%s:%s" %(schema,table)
+                                where = []
+                                update = []
+                                set_vals = row["after_values"]
+                                where_vals = row["before_values"]
+                                update = prepare_update_clause(set_vals)
+                                where = prepare_where_clause(where_vals)
+                                SQL = 'update  `'+schema+'`.`'+table+'` set '+" , ".join(update)+' where ' + " and ".join(where)
+                                logger.debug( 'Update SQL : '+SQL)
+                                # r.set(name = prefix+"update - ", value = SQL)
+                                push_mesage(channel,SQL,queue)
+                                log_position = stream.log_pos
+                            elif isinstance(binlogevent,WriteRowsEvent):
+                                prefix = "%s:%s" %(schema,table)
+                                vals = row["values"]
+                                insert = prepare_insert_values(vals)
+                                SQL = 'insert into  `'+schema+'`.`'+table+'` ('+" , ".join(insert['cols'])+') values ('+" , ".join(insert['vals'])+')'
+                                logger.debug( 'Insert SQL : '+SQL)
+                                # r.set(name = prefix+"insert - ", value = SQL)
+                                push_mesage(channel,SQL,queue)
+                                log_position = stream.log_pos
 
 
-
-            print "log position = %s" %(stream.log_pos)
+                log_position = stream.log_pos
+            logger.info( "log position = %s" %(log_position))
         sleep(10)
     stream.close()
     connection.close();
@@ -129,16 +142,12 @@ def prepare_where_clause(set_vals):
     return where
 
 
-
-
 def prepare_update_clause(set_vals):
     update = []
     for keys in set_vals:
         column = check_field_types(set_vals[keys])
         update.append(' '+keys+'= '+column)
     return update
-
-
 
 def prepare_insert_values(set_vals):
     insert = {}
@@ -152,17 +161,41 @@ def prepare_insert_values(set_vals):
     insert['vals'] = insert_vals
     return insert
 
-
-
 def check_field_types(column):
     column_str = None
     if type(column) == int:
         column_str = str(column)
     elif type(column) == bool:
         column_str = str(column)
+    elif type(column) == str:
+        column_str = '\''+str(column)+'\''
     else:
+        logger.warn('New Column Type found - %s - ' %(type(column)))
         column_str = '\''+str(column)+'\''
     return column_str
 
+def get_latest_master_log_file():
+    connection = None
+    log_file = '0'
+    try:
+        connection = mysql.connector.connect(user=MYSQL_SETTING['user'], password=MYSQL_SETTING['passwd'],
+                                  host=MYSQL_SETTING['host'],
+                                  database='alienvault')
+
+        cursor = connection.cursor()
+        cursor.execute("SHOW MASTER STATUS")
+        for rows in cursor:
+            log_file = rows[0]
+
+        cursor.close()
+        connection.close()
+    except mysql.connector.errors as mysqlerror:
+        logger.error(mysqlerror.msg)
+    finally:
+        if None != connection:
+            connection.close()
+
+    logger.debug('Log File -> %s' %(log_file))
+    return log_file
 if __name__ == "__main__":
     main()
