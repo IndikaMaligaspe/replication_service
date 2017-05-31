20ade5cbceaeb922ba6e152b30584946b287bb43
IndikaM <indikamalirezg@gmail.com>
2017-06-01 05:10:02 +0530

    started setup process changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+setup(
+    name = 'MySQL_RabbitMQ_replicator',
+    version='0.1',
+    description='A MySQL replication service using RabbitMQ',
+    url='',
+    autho='IndikaM',
+    author_mail='k.indika.maligaspe@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[]
+)
